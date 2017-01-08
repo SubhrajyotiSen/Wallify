2dc84f1ded65c826bf0a142f5edc5087ac142d7f
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-01-08 12:24:42 +0530

    feat: add About section with GitHub link

--- app/src/main/java/com/subhrajyoti/wallify/SettingsActivity.java
+++ app/src/main/java/com/subhrajyoti/wallify/SettingsActivity.java
@@ -1,8 +1,12 @@
 package com.subhrajyoti.wallify;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -52,7 +56,36 @@ public class SettingsActivity extends AppCompatActivity {
             findPreference("about").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle("Wallify");
+                    builder.setMessage("A randomized wallpaper app");
 
+                    String positiveText = getString(android.R.string.ok);
+                    builder.setPositiveButton(positiveText,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // positive button logic
+                                }
+                            });
+
+                    String negativeText = "GitHub";
+                    builder.setNegativeButton(negativeText,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // negative button logic
+                                    String url = "https://github.com/SubhrajyotiSen/Wallify";
+                                    Intent i = new Intent(Intent.ACTION_VIEW);
+                                    i.setData(Uri.parse(url));
+                                    startActivity(i);
+
+                                }
+                            });
+
+                    AlertDialog dialog = builder.create();
+                    // display dialog
+                    dialog.show();
                     return false;
                 }
             });
