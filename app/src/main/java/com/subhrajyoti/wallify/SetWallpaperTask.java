7d56cb1346840fc81c94be25eb86ccb7310788f9
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2017-01-23 22:52:44 +0530

    fix: Save Picasso Target from Garbage Collector

--- app/src/main/java/com/subhrajyoti/wallify/SetWallpaperTask.java
+++ app/src/main/java/com/subhrajyoti/wallify/SetWallpaperTask.java
@@ -12,7 +12,7 @@ public class SetWallpaperTask extends AsyncTask<Bitmap, Void, Boolean> {
         boolean status= true;
         final Bitmap bitmap = params[0];
         try {
-            Utils.getWallpapermanager().setBitmap(bitmap);
+            Utils.getWallpaperManager().setBitmap(bitmap);
         } catch (IOException e) {
             status = false;
             e.printStackTrace();
